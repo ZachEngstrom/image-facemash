b17afe57c5beaa09728bcab56af7fad9da7bf0b9
Zach Engstrom <zach.webdeveloper@gmail.com>
2025-02-20 02:02:47 -0600

    added a settings link to the plugin action links in the plugins list table

--- image-facemash.php
+++ image-facemash.php
@@ -24,3 +24,21 @@ require_once dirname(__FILE__) . '/inc/db.php';
 require_once dirname(__FILE__) . '/inc/admin.php';
 require_once dirname(__FILE__) . '/inc/frontend.php';
 require_once dirname(__FILE__) . '/inc/ajax.php';
+
+/**
+ * Adds a settings link to the plugin action links in the Plugins list table.
+ *
+ * @param array $links Existing action links for the plugin
+ * @return array Modified action links with settings added
+ */
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'image_facemash_add_settings_link');
+function image_facemash_add_settings_link(array $links): array {
+  // Create the settings page URL
+  $settings_url = admin_url('options-general.php?page=image-facemash-settings');
+
+  // Add the "Settings" link before existing links
+  $settings_link = '<a href="' . esc_url($settings_url) . '">' . __('Settings', 'image-facemash') . '</a>';
+  array_unshift($links, $settings_link);
+
+  return $links;
+}
